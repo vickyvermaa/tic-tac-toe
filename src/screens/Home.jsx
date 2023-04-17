1b5bf827ddab50b57b039d38a2dbac8cb6b4cf51
Vicky Verma <vickyvermaa22@gmail.com>
2023-04-17 17:17:21 +0530

    buttuon deleted

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 const Home = (props) => {
   return (
     <div className="home-buttons">
-      <button>Play with computer</button>
       <Link to='/playground'><button>Play with friends</button></Link>
     </div>
   );
